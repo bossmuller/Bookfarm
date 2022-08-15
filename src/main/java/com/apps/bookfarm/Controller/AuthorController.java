1b7a896d41fe9f8e7f4f6af624dde251d07dc0ff
bossmuller <bossvill.imbosa@gmail.com>
2022-08-15 20:17:03 +0300

    Uploaded successfully

--- src/main/java/com/apps/bookfarm/Controller/AuthorController.java
+++ src/main/java/com/apps/bookfarm/Controller/AuthorController.java
@@ -4,18 +4,37 @@ import com.apps.bookfarm.Model.Author;
 import com.apps.bookfarm.Repository.AuthorRepository;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.Optional;
 
 @RestController
-public class AuthorController {
+public class AuthorController{
     private final AuthorRepository authorRepository;
 
 
     public AuthorController(AuthorRepository authorRepository) {
+
         this.authorRepository = authorRepository;
     }
     @GetMapping("/authors")
-    List<Author>allAuthors(){
+    Iterable<Author>allAuthors(){
+
         return authorRepository.findAll();
     }
+    @GetMapping("/authors/{Id}")
+    Optional<Author> oneAuthor(@PathVariable Long Id){
+        return authorRepository.findById(Id);
+
+    }
+    @PostMapping("/authors")
+    Author newAuthor(@RequestBody Author newAuthor){
+        return authorRepository.save(newAuthor);
+    }
+    //@PutMapping("/authors/{Id}")
+   // Author replaceAuthor(@RequestBody Author newAuthor,@PathVariable Long Id){
+      //  return authorRepository.findById(Id).map() -> {
+           // newAuthor.setAuthorName(newAuthor.getAuthorName());
+            //newAuthor.setPhoneNumber(newAuthor.getPhoneNumber());
+            //return authorRepository.save(newAuthor);
+       // }
+   // }
 }
